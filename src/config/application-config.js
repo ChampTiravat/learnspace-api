48fa888d643af58d1a51c26b34074a179ebfbb84
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-27 22:46:22 +0700

    :sparkles: Setup app's configs for docker containerization process

--- src/config/application-config.js
+++ src/config/application-config.js
@@ -4,5 +4,5 @@ dotenv.config()
 
 // Application Configuration
 export const APP_PORT = 5000 || process.env.APP_PORT
-export const APP_HOST = `http://localhost`
+export const APP_HOST = `0.0.0.0` || process.env.APP_HOST
 export const APP_SERVING_PATH = `${APP_HOST}:${APP_PORT}`
