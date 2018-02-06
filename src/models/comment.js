704e9841f1576cae8fced4ed9c1cc181ac728875
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-02-06 20:27:22 +0700

    :sparkles: Added timestamps

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,26 +1,28 @@
 import mongoose, { Schema } from 'mongoose'
 
 const CommentSchema = new Schema({
-	creator: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	message: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	parentPost: {
-		type: Schema.Types.ObjectId,
-		ref: 'Post'
-	},
-	subComments: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'SubComment'
-		}
-	]
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  parentPost: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  },
+  subComments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'SubComment'
+    }
+  ],
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() }
 })
 
 export default mongoose.model('comments', CommentSchema)
