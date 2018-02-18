7c5706765a165764df1fbcc61fa009d7c7cfe458
tiravat <tiravat2016@gmail.com>
2018-02-18 22:22:47 +0700

    :zap: Adjusted some configuration

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -4,8 +4,8 @@ dotenv.config()
 
 // MongoDB Database Configurations
 export const MONGODB_DEV_HOST = process.env.MONGODB_DEV_HOST
-export const MONGODB_DEV_DBNAME = process.env.MONGODB_DEV_DBNAME
-export const MONGODB_DEV_PORT = process.env.MONGODB_DEV_PORT
+export const MONGODB_DEV_DBNAME = process.env.MONGODB_DEV_DBNAME 
+export const MONGODB_DEV_PORT = process.env.MONGODB_DEV_PORT || ''
 export const DB_CONNECTION_STRING =`mongodb://${MONGODB_DEV_HOST}${MONGODB_DEV_PORT}/${MONGODB_DEV_DBNAME}`
 
 // Redis Database Configurations
