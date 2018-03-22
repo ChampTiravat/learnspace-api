4ca3c9be172398664dc7091a8389d3bfa3476382
tiravat <tiravat2016@gmail.com>
2018-03-22 11:25:54 +0700

    :hammer: Refactored

--- src/handlers/classroom/mutation/create-classroom.js
+++ src/handlers/classroom/mutation/create-classroom.js
@@ -1,7 +1,17 @@
 import { isEmpty, trim, isMongoId } from 'validator'
 
+import { requiredAuthentication } from '../../../helpers/security-helpers'
 import { displayErrMessageWhenDev } from '../../../helpers/error-helpers'
 import { ENG_THA_NUM_ALPHA } from '../../../constants/regex-patterns'
+
+const formatGraphQLErrorMessage = message => ({
+  success: false,
+  classroomID: '',
+  err: {
+    name: 'createClassroom',
+    message
+  }
+})
 
 /** ==================================================================================
  * @name createClassroom()
@@ -18,63 +28,30 @@ import { ENG_THA_NUM_ALPHA } from '../../../constants/regex-patterns'
 export default async (_, { name, description, subject }, { user, models }) => {
   try {
     // User must be authenticated
-    if (isEmpty(trim(user._id)) || !isMongoId(user._id)) {
-      return {
-        success: false,
-        classroomID: '',
-        err: {
-          name: 'createClassroom',
-          message: 'Unauthorized Access'
-        }
-      }
-    }
+    if (!requiredAuthentication)
+      return formatGraphQLErrorMessage('Authentication Required')
 
     // All GraphQL Mutation parameters are required
     if (
       isEmpty(trim(name)) ||
-      isEmpty(trim(description)) ||
-      isEmpty(trim(subject))
-    ) {
-      return {
-        success: false,
-        classroomID: '',
-        err: {
-          name: 'createClassroom',
-          message: 'Important information should not be empty'
-        }
-      }
-    }
+      isEmpty(trim(subject)) ||
+      isEmpty(trim(description))
+    )
+      return formatGraphQLErrorMessage(
+        'Important information should not be empty'
+      )
 
     // Validation
     if (!ENG_THA_NUM_ALPHA.test(name))
-      return {
-        success: false,
-        classroomID: '',
-        err: {
-          name: 'createClassroom',
-          message: "'name' is invalid or not specified"
-        }
-      }
+      return formatGraphQLErrorMessage("'name' is invalid or not specified")
 
     if (!ENG_THA_NUM_ALPHA.test(description))
-      return {
-        success: false,
-        classroomID: '',
-        err: {
-          name: 'createClassroom',
-          message: "'description' is invalid or not specified"
-        }
-      }
+      return formatGraphQLErrorMessage(
+        "'description' is invalid or not specified"
+      )
 
     if (!ENG_THA_NUM_ALPHA.test(subject))
-      return {
-        success: false,
-        classroomID: '',
-        err: {
-          name: 'createClassroom',
-          message: "'subject' is invalid or not specified"
-        }
-      }
+      return formatGraphQLErrorMessage("'subject' is invalid or not specified")
 
     // New Classroom Construction
     const newClassroom = await models.Classroom.create({
@@ -87,27 +64,18 @@ export default async (_, { name, description, subject }, { user, models }) => {
     // Add user(current user who created the classroom above)
     // as a member of the classroom
     await models.ClassroomMember.create({
-      member: user._id,
       classroom: newClassroom._id,
+      member: user._id,
       role: 'admin'
     })
 
     return {
-      success: true,
       classroomID: newClassroom._id,
+      success: true,
       err: null
     }
   } catch (err) {
-    // Some Error Occured
     displayErrMessageWhenDev(err)
-
-    return {
-      success: false,
-      classroomID: '',
-      err: {
-        name: 'createClassroom',
-        message: 'Server Error'
-      }
-    }
+    return formatGraphQLErrorMessage('Server Error')
   }
 }
