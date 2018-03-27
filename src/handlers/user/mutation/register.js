109499d0da0110e8a67aad6ba7d25bc94d7d42f5
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-03-27 16:28:43 +0700

    :hammer: Beautified and shorten code

--- src/handlers/user/mutation/register.js
+++ src/handlers/user/mutation/register.js
@@ -1,10 +1,20 @@
 import { equals, isEmail, isEmpty, trim, isAlphanumeric } from 'validator'
 import bcrypt from 'bcrypt'
 
+import { displayErrMessageWhenDev } from '../../../helpers/error-helpers'
 import {
   ENG_THA_NUM_ALPHA,
   PASSWORD_PATTERN
 } from '../../../constants/regex-patterns'
+
+const formatGraphQLErrorMessage = message => ({
+  success: false,
+  user: null,
+  err: {
+    name: 'register',
+    message
+  }
+})
 
 /** ==================================================================================
  * @name register()
@@ -25,7 +35,9 @@ export default async (
   { models }
 ) => {
   try {
+    // =========================================================
     // Inputs Validation
+    // =========================================================
     if (
       isEmpty(trim(username)) ||
       isEmpty(trim(fname)) ||
@@ -33,129 +45,70 @@ export default async (
       isEmpty(trim(lname)) ||
       isEmpty(trim(password))
     ) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Important credentials should not be empty'
-        }
-      }
+      return formatGraphqlErrorMessage(
+        'Important credentials should not be empty'
+      )
     }
 
     // Email format Validation
-    if (!isEmail(email) || email.length > 250) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Email is not valid'
-        }
-      }
-    }
+    if (!isEmail(email) || email.length > 250)
+      return formatGraphqlErrorMessage('Email is not valid')
 
-    // Username must contains only ENG alphabets
-    // and must be only 1 - 30 characters
-    if (!isAlphanumeric(username) || username.length > 50) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Username is not valid'
-        }
-      }
-    }
+    // Username must contains only ENG alphabets and must be only 1 - 30 characters
+    if (!isAlphanumeric(username) || username.length > 50)
+      return formatGraphqlErrorMessage('Username is not valid')
 
-    // Firstname must contains only ENG or THA alphabets
-    // and must be only 1 - 50 characters
-    if (!ENG_THA_NUM_ALPHA.test(fname) || fname.length > 50) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Firstname is not valid'
-        }
-      }
-    }
+    // Firstname must contains only ENG or THA alphabets and must be only 1 - 50 characters
+    if (!ENG_THA_NUM_ALPHA.test(fname) || fname.length > 50)
+      return formatGraphqlErrorMessage('Firstname is not valid')
 
-    // Lastname must contains only ENG or THA alphabets
-    // and must be only 1 - 50 characters
-    if (!ENG_THA_NUM_ALPHA.test(lname) || lname.length > 50) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Lastname is not valid'
-        }
-      }
-    }
+    // Lastname must contains only ENG or THA alphabets and must be only 1 - 50 characters
+    if (!ENG_THA_NUM_ALPHA.test(lname) || lname.length > 50)
+      return formatGraphqlErrorMessage('Lastname is not valid')
 
-    // Password may contain anything and must have
-    // a length between 8 - 55 characters
-    if (!PASSWORD_PATTERN.test(password) || password.length > 55) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Password is not valid'
-        }
-      }
-    }
+    // Password may contain anything and must have a length between 8 - 55 characters
+    if (!PASSWORD_PATTERN.test(password) || password.length > 55)
+      return formatGraphqlErrorMessage('Password is not valid')
 
     // Password Validation
-    if (password.length < 8) {
-      return {
-        success: false,
-        user: null,
-        err: {
-          name: 'register',
-          message: 'Password is too weak, must be >= 8, but <= 250 characters'
-        }
-      }
-    }
+    if (password.length < 8)
+      return formatGraphqlErrorMessage(
+        'Password is too weak, must be >= 8, but <= 250 characters'
+      )
 
+    // =========================================================
     // Checking wether user is already exists
+    // =========================================================
     const userWithTheSameCreds = await models.User.findOne({
       $or: [{ email: email }, { username: username }]
     })
 
-    // User email must be unique
+    // =========================================================
+    // User email and username must be unique. So, we're checking for a conflict
+    // =========================================================
     if (userWithTheSameCreds) {
       // Email conflict
-      if (equals(userWithTheSameCreds.email, email)) {
-        return {
-          success: false,
-          user: null,
-          err: {
-            name: 'register',
-            message: 'User already exist with the given email'
-          }
-        }
-      }
+      if (equals(userWithTheSameCreds.email, email))
+        return formatGraphqlErrorMessage(
+          'User already exist with the given email'
+        )
 
       // Username conflict
-      if (equals(userWithTheSameCreds.username, username)) {
-        return {
-          success: false,
-          user: null,
-          err: {
-            name: 'register',
-            message: 'User already exist with the given username'
-          }
-        }
-      }
+      if (equals(userWithTheSameCreds.username, username))
+        return formatGraphqlErrorMessage(
+          'User already exist with the given username'
+        )
     }
 
+    // =========================================================
     // Encrypt Password
+    // =========================================================
     const salt = await bcrypt.genSaltSync(12)
     const hashedPassword = await bcrypt.hashSync(password, salt)
 
+    // =========================================================
     // Saving new user
+    // =========================================================
     const user = await models.User.create({
       username,
       fname,
@@ -164,19 +117,15 @@ export default async (
       password: hashedPassword
     })
 
+    // =========================================================
+    // Return appropriete GraphQL response
+    // =========================================================
     return {
       success: true,
       user
     }
   } catch (err) {
-    console.log(err)
-    return {
-      success: false,
-      user: null,
-      err: {
-        name: 'register',
-        message: 'Server Error'
-      }
-    }
+    displayErrMessageWhenDev(err)
+    return formatGraphQLErrorMessage('Server Error')
   }
 }
