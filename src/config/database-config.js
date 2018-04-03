f642a11235a30d702809f7176b7e8978781c9523
tiravat <tiravat2016@gmail.com>
2018-04-04 05:56:15 +0700

    :hammer: Changed config schemes

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const isTesting = !!process.env.TESTING
+
 // MongoDB Database Configurations
-export const MONGODB_DEV_HOST = process.env.MONGODB_DEV_HOST
-export const MONGODB_DEV_DBNAME = process.env.MONGODB_DEV_DBNAME
-export const MONGODB_DEV_PORT = process.env.MONGODB_DEV_PORT || ''
-export const DB_CONNECTION_STRING = `mongodb://${MONGODB_DEV_HOST}:${MONGODB_DEV_PORT}/${MONGODB_DEV_DBNAME}`
+export const MONGODB_HOST 	  = isTesting ? process.env.MONGODB_TEST_HOST : process.env.MONGODB_HOST
+export const MONGODB_PORT     	  = isTesting ? process.env.MONGODB_TEST_PORT : process.env.MONGODB_PORT
+export const MONGODB_DBNAME       = isTesting ? process.env.MONGODB_TEST_DBNAME: process.env.MONGODB_DBNAME
+export const DB_CONNECTION_STRING = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DBNAME}`
 
 // Redis Database Configurations
-export const REDIS_HOST = process.env.REDIS_HOST
-export const REDIS_PORT = process.env.REDIS_PORT
+export const REDIS_HOST = isTesting ? process.env.REDIS_TEST_HOST : process.env.REDIS_HOST
+export const REDIS_PORT = isTesting ? process.env.REDIS_TEST_PORT : process.env.REDIS_PORT
