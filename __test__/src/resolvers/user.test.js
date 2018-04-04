fc29b48639f8344dac5afc57c4db1aee52fd9304
tiravat <tiravat2016@gmail.com>
2018-04-04 20:35:21 +0700

    :hammer: Using superagent instead of axios

--- __test__/src/resolvers/user.test.js
+++ __test__/src/resolvers/user.test.js
@@ -1,85 +1,55 @@
-import axios from 'axios'
+import request from 'superagent'
 
 import { TEST_URL } from '../../test-config'
 import User from '../../../src/models/user'
 
-beforeEach(() => {
-	return Promise.resolve(User.remove({}))
-})
+describe('Testing User Resolver', async () => {
+  beforeEach(() => {
+    return Promise.resolve(User.remove({}))
+  })
 
-describe('Testing User Resolver', () => {
-	test('User Registration', async () => {
-		const response = await axios.post(TEST_URL, {
-			query: `
-        mutation {
-          register(
-            email: "bw@marvel.com",
-            password: "this is a very secure password",
-            fname: "Scarlett",
-            lname: "Johanson"
-          ) {
-            success
-            err
-            user {
-              fname
-              lname
-              email
-            }
-          }
-        }
+  it('User Registration', async done => {
+    const response = await request.post(TEST_URL).send({
+      query: `
+		        mutation {
+			        register(
+				        email: "tiravat2016@gmail.com",
+				        username: "tiravat2016",
+				        fname: "tiravat",
+				        lname: "thaisubvorakul",
+				        password: "123456789"
+			        ) {
+				        success
+					      user {
+						      email
+						      username
+						      fname
+						      lname
+					      }
+					      err {
+						      message
+					      }
+			        }
+		        }
       `
-		})
-		const expectResult = {
-			data: {
-				register: {
-					success: true,
-					err: null,
-					user: {
-						fname: 'Scarlett',
-						lname: 'Johanson',
-						email: 'bw@marvel.com'
-					}
-				}
-			}
-		}
+    })
 
-		expect(response.data).toMatchObject(expectResult)
-	})
-
-	test('User Login', async () => {
-		const response = await axios.post(TEST_URL, {
-			query: `
-        mutation {
-          login(email: "bw@marvel.com", password: "this is a very secure password") {
-            success
-
-            user {
-              fname
-              lname
-              email
-            }
-            err
-          }
+    const expectedResult = {
+      data: {
+        register: {
+          success: true,
+          user: {
+            email: 'tiravat2016@gmail.com',
+            username: 'tiravat2016',
+            fname: 'tiravat',
+            lname: 'thaisubvorakul'
+          },
+          err: null
         }
-      `
-		})
+      }
+    }
 
-		const expectResult = {
-			data: {
-				login: {
-					success: true,
-					// token:
-					// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJ3QG1hcnZlbC5jb20iLCJpYXQiOjE1MTU2MDEzMzgsImV4cCI6MTUxNTYwODUzOH0.iWfRTW7BQ27ozOXP9AZTsw_MFTIYneK8mrUcaF1Fcn8',
-					user: {
-						fname: 'Scarlett',
-						lname: 'Johanson',
-						email: 'bw@marvel.com'
-					},
-					err: ''
-				}
-			}
-		}
-
-		expect(response.data).toMatchObject(expectResult)
-	})
+    expect(JSON.parse(response.text)).toMatchObject(expectedResult)
+    done()
+  })
 })
